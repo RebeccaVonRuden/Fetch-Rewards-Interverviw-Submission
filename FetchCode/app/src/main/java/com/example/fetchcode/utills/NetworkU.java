f333f784e4955e4acf10c827ce637c543e0e1e22
RebeccaVonRuden <rebecca@vonruden.info>
2023-06-26 20:57:16 -0500

    Remove Network.java
    I was confused by the name so deleted old file and created new

--- FetchCode/app/src/main/java/com/example/fetchcode/utills/NetworkU.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/utills/NetworkU.java
@@ -6,19 +6,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-public class Network {
+public class NetworkU{
     public static String makeHTTPrequest(URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         InputStream inputStream = connection.getInputStream();
 
         try{
-        Scanner scanner = new Scanner(inputStream);
-        scanner.useDelimiter("\\A");
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
 
-        boolean hasInput = scanner.hasNext();
-        if(hasInput)
-            return scanner.next();
-        else return null;
+            boolean hasInput = scanner.hasNext();
+            if(hasInput)
+                return scanner.next();
+            else return null;
         }
         finally {
             connection.disconnect();
