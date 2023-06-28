19b0870fe238df5afdbf129212424f2b83c6d21b
RebeccaVonRuden <rebecca@vonruden.info>
2023-06-28 11:15:51 -0500

    Version2 Using ListView
    Update the code to be using the ListView object. Can read the API and put items in the list. I need to fix the display and add sort.  The TextView is still in the current code. I plan on removing it when the listView is working completely.

--- FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
@@ -1,7 +1,38 @@
 package com.example.fetchcode.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Item {
-    Integer id;
-    String listID;
+    @SerializedName("id")
+    int id;
+    @SerializedName("listId")
+    int listId;
+    @SerializedName("name")
     String name;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setId(int newId){
+        id = newId;
+    }
+
+    public void setListId(int newListId){
+        listId = newListId;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
 }
