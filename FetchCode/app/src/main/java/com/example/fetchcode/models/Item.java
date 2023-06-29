49b96a5c7f3cb7562b1d1dceac94ecd287b83f0c
RebeccaVonRuden <rebecca@vonruden.info>
2023-06-29 15:12:20 -0500

    Reorganize Item.java
    Reorganized Item.java
    
    Moved equals and hashCode to the top of class
    Removed old sort code - Still trying to get sort to work

--- FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
@@ -39,18 +39,20 @@ public class Item {
         name = newName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && listId == item.listId && Objects.equals(name, item.name);
+    }
 
-    public static Comparator<Item> ItemListIdSort = new Comparator<Item>() {
-        @Override
-        public int compare(Item item, Item i1) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, listId, name);
+    }
 
-            int id1 = Integer.valueOf(item.getListId());
-            int id2 = Integer.valueOf(i1.getListId());
-            return Integer.compare(id1, id2);
-        }
-
-    };
-
+    // Trying to get sort to work with Name //Call//    Collections.sort(itemList, Item.ItemNameSort)
     public static Comparator<Item> ItemNameSort = new Comparator<Item>() {
         @Override
         public int compare(Item item1, Item item2) {
@@ -69,33 +71,4 @@ public class Item {
             }
         }
     };
-    public static Comparator<Item> ItemListIdName = new Comparator<Item>() {
-        @Override
-        public int compare(Item item, Item i1) {
-            int id1 = Integer.valueOf(item.getListId());
-            int id2 = Integer.valueOf(i1.getListId());
-            String name1 = item.getName();
-            String name2 = item.getName();
-            name1 = name1.toLowerCase();
-            name2 = name2.toLowerCase();
-            if(Integer.compare(id1, id2) == 0){
-                return name1.compareTo(name2);
-            }
-            return Integer.compare(id1, id2);
-        }
-    };
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return id == item.id && listId == item.listId && Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, listId, name);
-    }
-
 }
