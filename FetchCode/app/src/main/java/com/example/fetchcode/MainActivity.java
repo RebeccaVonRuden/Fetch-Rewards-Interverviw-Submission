4eb7ded4168487aea7e4dcb2d99bcde153504db6
RebeccaVonRuden <rebecca@vonruden.info>
2023-06-29 13:06:29 -0500

    Group By ListId
    The list is now grouped by ListId.

--- FetchCode/app/src/main/java/com/example/fetchcode/MainActivity.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/MainActivity.java
@@ -10,10 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.fetchcode.models.Item;
 import com.example.fetchcode.utills.ApiService;
 
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -22,10 +22,8 @@ public class MainActivity extends AppCompatActivity {
     TextView Welcome;
     ListView list;
     ArrayList<Item> itemList;
-    ArrayList<String> itemlist2;
+//    ArrayAdapter<Item> listAdapt;
     ArrayAdapter<String> listAdapt;
-    ArrayAdapter<String> listAdapt2;
-    ArrayList<JSONObject>  allDataPoints;
     ArrayList<String> itemString;
 
 
@@ -35,47 +33,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Welcome = findViewById(R.id.Welcome);
         list = findViewById(R.id.list_Of_Res);
-        itemList = new ArrayList<Item>();
-        itemString = new ArrayList<String>();
-        createList();
-
+        itemList = new ArrayList<>();
+        itemString = new ArrayList<>();
 
 
         ApiService apiService = new ApiService();
         apiService.fetchDataFromApi(new ApiService.ApiCallback() {
             @Override
             public void onSuccess(List<Item> items) {
-                // Handle successful API response here
-//                Comparator<Item> compare = Comparator.comparing(Item -> Item.getId());
-//                compare = compare.thenComparing(Comparator.comparing(Item -> Item.getName()));
+                // Group items by listId
+                HashMap<Integer, List<Item>> groupedItems = groupItemsByListId(items);
 
-                for (Item item : items) {
-                    int id = item.getId();
-                    int listId = item.getListId();
-                        try {
-                            String name = item.getName();
-                            if (name != null && !name.isEmpty()) {
-                                    Item currentItem = new Item();
-                                    currentItem.setId(id);
-                                    currentItem.setListId(listId);
-                                    currentItem.setName(name);
-                                    itemList.add(currentItem);
-                                }
+                // Sort groups by name
+                List<List<Item>> sortedGroups = sortGroupsByName(groupedItems);
 
-                        } catch (NullPointerException e){
-                            System.err.println("NullPointerException occurred for item: " + item);
-                            // Continue the loop
-                    }
-                    Collections.sort(itemList);
-                    for(int i =0; i < itemList.size();i++){
-                        Item currentItem = itemList.get(i);
-                        String tempString;
-                        tempString = "List Id: " +  currentItem.getListId()  + " , Name:  " + currentItem.getName() + " , Id: " + currentItem.getId();
-                        itemString.add(tempString);
-                    }
-                }
-//                listAdapt = new ArrayAdapter<Item>(MainActivity.this, android.R.layout.simple_list_item_1, itemList);
-                listAdapt = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, itemString);
+                // Prepare sorted list for ListView
+                prepareListForListView(sortedGroups);
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -87,16 +61,79 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(String errorMessage) {
-                // Handle API request failure here
-                // Display an error message or retry the request
+                // Display an error message
                 throw new RuntimeException(errorMessage);
             }
         });
 
     }
 
-    private void createList() {
-        itemlist2 = new ArrayList<String>();
-        listAdapt2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, itemlist2);
+
+    public void sortByListID(){
+        Collections.sort(itemList,Item.ItemListIdSort);
+    }
+
+    public void sortByName(){
+        Collections.sort(itemList,Item.ItemNameSort);
+    }
+
+    // Group items by listId
+    private HashMap<Integer, List<Item>> groupItemsByListId(List<Item> items) {
+        HashMap<Integer, List<Item>> groupedItems = new HashMap<>();
+        for (Item item : items) {
+            int listId = item.getListId();
+            if (!groupedItems.containsKey(listId)) {
+                groupedItems.put(listId, new ArrayList<>());
+            }
+            groupedItems.get(listId).add(item);
+        }
+        return groupedItems;
+    }
+
+    // Sort groups by name
+    private List<List<Item>> sortGroupsByName(HashMap<Integer, List<Item>> groupedItems) {
+        List<List<Item>> sortedGroups = new ArrayList<>(groupedItems.values());
+        Comparator<List<Item>> groupComparator = new Comparator<List<Item>>() {
+            @Override
+            public int compare(List<Item> group1, List<Item> group2) {
+                if (group1.isEmpty() && group2.isEmpty()) {
+                    return 0;
+                } else if (group1.isEmpty()) {
+                    return -1;
+                } else if (group2.isEmpty()) {
+                    return 1;
+                } else {
+                    String name1 = group1.get(0).getName();
+                    String name2 = group2.get(0).getName();
+                    if (name1 == null && name2 == null) {
+                        return 0;
+                    } else if (name1 == null) {
+                        return -1;
+                    } else if (name2 == null) {
+                        return 1;
+                    } else {
+                        name1 = name1.toLowerCase();
+                        name2 = name2.toLowerCase();
+                        return name1.compareTo(name2);
+                    }
+                }
+            }
+        };
+        Collections.sort(sortedGroups, groupComparator);
+        return sortedGroups;
+    }
+
+    // Prepare sorted list for ListView
+    private void prepareListForListView(List<List<Item>> sortedGroups) {
+        for (List<Item> group : sortedGroups) {
+            for (Item item : group) {
+                String name = item.getName();
+                if (name != null && !name.isEmpty()) {
+                    String tempString = "List Id: " + item.getListId() + ", Name: " + item.getName() + ", Id: " + item.getId();
+                    itemString.add(tempString);
+                }
+            }
+        }
+        listAdapt = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, itemString);
     }
 }
--- FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/models/Item.java
@@ -2,7 +2,11 @@ package com.example.fetchcode.models;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Item implements Comparable<Item>{
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Item {
+//   implements  Comparable<Item>
     @SerializedName("id")
     int id;
     @SerializedName("listId")
@@ -36,18 +40,62 @@ public class Item implements Comparable<Item>{
     }
 
 
-//    @Override
-//    public int compareTo(Item item) {
-//        if (this.listId == item.listId) {
-//            return this.name.compareTo(item.name);
-//        } else {
-//            return Integer.compare(this.listId, item.listId);
-//        }
-//    }
+    public static Comparator<Item> ItemListIdSort = new Comparator<Item>() {
+        @Override
+        public int compare(Item item, Item i1) {
+
+            int id1 = Integer.valueOf(item.getListId());
+            int id2 = Integer.valueOf(i1.getListId());
+            return Integer.compare(id1, id2);
+        }
+
+    };
+
+    public static Comparator<Item> ItemNameSort = new Comparator<Item>() {
+        @Override
+        public int compare(Item item1, Item item2) {
+            String name1 = item1.getName();
+            String name2 = item2.getName();
+            if (name1 == null && name2 == null) {
+                return 0;
+            } else if (name1 == null) {
+                return -1;
+            } else if (name2 == null) {
+                return 1;
+            } else {
+                name1 = name1.toLowerCase();
+                name2 = name2.toLowerCase();
+                return name1.compareTo(name2);
+            }
+        }
+    };
+    public static Comparator<Item> ItemListIdName = new Comparator<Item>() {
+        @Override
+        public int compare(Item item, Item i1) {
+            int id1 = Integer.valueOf(item.getListId());
+            int id2 = Integer.valueOf(i1.getListId());
+            String name1 = item.getName();
+            String name2 = item.getName();
+            name1 = name1.toLowerCase();
+            name2 = name2.toLowerCase();
+            if(Integer.compare(id1, id2) == 0){
+                return name1.compareTo(name2);
+            }
+            return Integer.compare(id1, id2);
+        }
+    };
 
     @Override
-    public int compareTo(Item item) {
-        return this.getListId() > item.getListId() ? 1: (this.getListId() < item.getListId() ? -1 : 0);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && listId == item.listId && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, listId, name);
     }
 
 }
