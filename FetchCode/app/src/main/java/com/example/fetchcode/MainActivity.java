ba9e9c6375feb366ef33989f931c945a85e57664
RebeccaVonRuden <rebecca@vonruden.info>
2023-06-28 20:49:44 -0500

    Remove Textview
    Removed all code related to the Textview
    NetworkU deleted

--- FetchCode/app/src/main/java/com/example/fetchcode/MainActivity.java
+++ FetchCode/app/src/main/java/com/example/fetchcode/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.fetchcode;
 
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,29 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fetchcode.models.Item;
 import com.example.fetchcode.utills.ApiService;
-import com.example.fetchcode.utills.NetworkU;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     TextView Welcome;
-    TextView IdDataResult;
-    Button updateData;
     ListView list;
     ArrayList<Item> itemList;
     ArrayList<String> itemlist2;
-//    ArrayAdapter<Item> listAdapt;
     ArrayAdapter<String> listAdapt;
     ArrayAdapter<String> listAdapt2;
     ArrayList<JSONObject>  allDataPoints;
@@ -47,8 +34,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Welcome = findViewById(R.id.Welcome);
-        IdDataResult = findViewById(R.id.Id_Data_Result);
-        updateData = findViewById(R.id.update_data);
         list = findViewById(R.id.list_Of_Res);
         itemList = new ArrayList<Item>();
         itemString = new ArrayList<String>();
@@ -108,99 +93,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        updateData.setOnClickListener(this);
     }
 
     private void createList() {
         itemlist2 = new ArrayList<String>();
         listAdapt2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, itemlist2);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.update_data){
-            try {
-                getData();
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private void getData() throws MalformedURLException {
-        Uri uri = Uri.parse("https://fetch-hiring.s3.amazonaws.com/hiring.json")
-                .buildUpon().build();
-        URL url = new URL(uri.toString());
-        new accessAPI().execute(url);
-    }
-
-    class accessAPI extends AsyncTask<URL,Void,String>{
-        @Override
-        protected String doInBackground(URL... urls) {
-            URL url = urls[0];
-            String data = null;
-            try{
-                data = NetworkU.makeHTTPrequest(url);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return  data;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            if (!s.isEmpty()) {
-                try {
-                    parseJson(s);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        public void parseJson(String data) throws JSONException {
-;
-            JSONArray jsonArray = null;
-            allDataPoints = new ArrayList<JSONObject>();
-            JSONObject current = null;
-
-            try{
-                jsonArray = new JSONArray(data);
-            } catch ( JSONException e) {
-                e.printStackTrace();
-            }
-            try {
-                for(int i = 0; i < jsonArray.length(); i++)
-                {
-                    JSONObject currentObject = jsonArray.getJSONObject(i);
-                    allDataPoints.add(currentObject);
-
-                }
-            } catch (JSONException e){
-                e.printStackTrace();
-            }
-
-            for (int i = 0; i < allDataPoints.size();i++) {
-                current = allDataPoints.get(i);
-                if(current.getString("name") != "null" || current.getString("name") != null){
-                    String name = current.getString("name");
-                    // filter out unwanted data
-                    if (name != "" == true) {
-                        allDataPoints.remove(i);
-                    }
-                    else{
-                        itemlist2.add(allDataPoints.get(i).toString());
-                    }
-                }
-            }
-            MainActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    IdDataResult.setText(allDataPoints.toString());
-                    list.setAdapter(listAdapt2);
-                    listAdapt2.notifyDataSetChanged();
-                }
-            });
-
-        }
     }
 }
